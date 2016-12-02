2592eb03320dbaecd70d0616890199f6bd286545
Daniel Oaks <doaks@londontrustmedia.com>
2016-12-03 02:27:19 +1000

    Initial s2s protocol docs

---
+++ lib/s2s/doc.go
@@ -0,0 +1,12 @@
+/*
+Package s2s provides IRC Server-to-Server protocol modules.
+
+This allows you to have a single simplified interface that can be used to
+interact with a number of different IRC servers. The upside of this is that
+you can implement code once and have it work across a number of different
+servers.
+
+The main intent with this package is to develop InspIRCd support first, and to
+make it easy to implement support for other IRCds later down the track.
+*/
+package s2s
