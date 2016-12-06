f90a0a9d4b7530ce1939b8760c90b98e83a13c19
Daniel Oaks <doaks@londontrustmedia.com>
2016-12-06 11:15:44 +1000

    Start db package

---
+++ lib/db/doc.go
@@ -0,0 +1,11 @@
+// written by London Trust Media
+// released under the MIT license
+
+/*
+Package db provides Veritas database modules.
+
+We have a default db module we prefer. However, this lets us easily write new
+database backends and plug them into Veritas. We'll do this if later on we find
+the primary approach doesn't work well for us or similar.
+*/
+package db
