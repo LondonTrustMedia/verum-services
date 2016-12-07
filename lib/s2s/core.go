344459c6be8ee416c2bd46cff4abf0fa3ce34c42
Daniel Oaks <doaks@londontrustmedia.com>
2016-12-07 16:48:30 +1000

    Rough S2S stuff

--- lib/s2s/core.go
+++ lib/s2s/core.go
@@ -2,8 +2,27 @@
 // released under the MIT license
 package s2s
 
+import (
+	"strings"
+
+	"github.com/Verum/veritas/lib"
+)
+
 // Protocol is the core S2S protocol interface that is implemented by all S2S protos.
 type Protocol interface {
 	Run()
 	AddClient(nick, user, host, realname string)
+}
+
+// MakeProro returns a protocol module given the config.
+func MakeProro(config *lib.Config) (Protocol, error) {
+	var p *Protocol
+
+	protoName := strings.ToLower(config.IRCd.Module)
+	if protoName == "inspircd" {
+		inspProto, err := makeInsp(config)
+		return inspProto, err
+	}
+
+	return p, nil
 }
--- lib/s2s/inspircd.go
+++ lib/s2s/inspircd.go
@@ -2,8 +2,16 @@
 // released under the MIT license
 package s2s
 
+import "github.com/DanielOaks/girc-go/ircmap"
+import "github.com/Verum/veritas/lib"
+
 // InspIRCd is the S2S protocol module for Insp.
 type InspIRCd struct {
+	Casemapping ircmap.MappingType
+}
+
+func makeInsp(config *lib.Config) (*InspIRCd, error) {
+
 }
 
 func (p *InspIRCd) Run() {
