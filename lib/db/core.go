1cf80205364f9b4165cc98bbd799c3851acabc33
Daniel Oaks <doaks@londontrustmedia.com>
2016-12-07 22:30:26 +1000

    db: Add impl note

--- lib/db/core.go
+++ lib/db/core.go
@@ -4,8 +4,9 @@ package db
 
 // DB is the core database interface. Our DBs are based on key-value storage.
 type DB interface {
-	Get(key string) (string, error)
-	Set(key string, value string) error
+	// use same interface as buntdb -- should work nicely across different providers
+	// Get(key string) (string, error)
+	// Set(key string, value string) error
 	Upgrade() error
 	Save(filename string) error // ignored for ones that don't use files
 }
